4c06cbcb379e746a4346a18c5289054164baf0f4
20A3172 <93903096+20A3172@users.noreply.github.com>
2022-05-24 12:47:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -19,55 +19,56 @@ for i in range(1,100):
     time.sleep(load_time)
 st.write('Finish Loading!')
 
-# 画像表示
-st.write('Display Image:')
-img = Image.open('Emily.jpg')
-if st.checkbox('show Image'):
-    st.image(img,caption='Emily',use_column_width=True)
+while 1:
+    # 画像表示
+    st.write('Display Image:')
+    img = Image.open('Emily.jpg')
+    if st.checkbox('show Image'):
+        st.image(img,caption='Emily',use_column_width=True)
 
 
-st.sidebar.write('Interactive Wdgets')
+    st.sidebar.write('Interactive Wdgets')
 
-# ボタン押したら表示
-left_colum,right_colum = st.columns(2)
-button = left_colum.button('display right')
-if button:
-    right_colum.write('right colum_here')
+    # ボタン押したら表示
+    left_colum,right_colum = st.columns(2)
+    button = left_colum.button('display right')
+    if button:
+        right_colum.write('right colum_here')
 
-# クリックしたら表示する
-expander = st.expander('cellphon')
-expander.write('call back cellphon')
+    # クリックしたら表示する
+    expander = st.expander('cellphon')
+    expander.write('call back cellphon')
 
-expander1 = st.expander('cellphon2')
-expander1.write('call back cellphon2')
+    expander1 = st.expander('cellphon2')
+    expander1.write('call back cellphon2')
 
-expander2 = st.expander('cellphon3')
-expander2.write('call back cellphon3')
+    expander2 = st.expander('cellphon3')
+    expander2.write('call back cellphon3')
 
-# 入力
-hobby = st.sidebar.text_input('tell me your hobby')
-# 特定範囲の数値入力
-fine = st.sidebar.slider('how are you?',0,100,50)
+    # 入力
+    hobby = st.sidebar.text_input('tell me your hobby')
+    # 特定範囲の数値入力
+    fine = st.sidebar.slider('how are you?',0,100,50)
 
-hobby,'!? me too!'
-'condition:',fine
+    hobby,'!? me too!'
+    'condition:',fine
 
-num = st.selectbox(
-    'あなたが好きな数字',
-    list(range(1,11))
-)    
-'you like ',num,'!'
+    num = st.selectbox(
+        'あなたが好きな数字',
+        list(range(1,11))
+    )    
+    'you like ',num,'!'
 
-# データフレームの表示
-st.write('DataFrame')
+    # データフレームの表示
+    st.write('DataFrame')
 
-df = pd.DataFrame(
-  np.random.rand(10,2),
-  columns=['a','b']
-)
+    df = pd.DataFrame(
+      np.random.rand(10,2),
+      columns=['a','b']
+    )
 
-st.table(df.style.highlight_max(axis=0)) #表で表示
-#st.line_chart(df) #線グラフで表示
-#st.area_chart(df) #塗りつぶし線グラフで表示
-#st.bar_chart(df) #棒グラフで表示
-#st.map(df)
+    st.table(df.style.highlight_max(axis=0)) #表で表示
+    #st.line_chart(df) #線グラフで表示
+    #st.area_chart(df) #塗りつぶし線グラフで表示
+    #st.bar_chart(df) #棒グラフで表示
+    #st.map(df)
